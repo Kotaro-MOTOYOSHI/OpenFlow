1be5146a2d197c0150df3aa12035bed30dee383f
SASAKI Takashi <sasaki.takashi.mg@linux1.cc.ehime-u.ac.jp>
2016-11-09 18:13:33 +0900

    removed harmful 'using namespace'

--- openflow-vlan-controller.h
+++ openflow-vlan-controller.h
@@ -1,7 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
-#define OPENFLOW_VLAN_CONTROLLER_H
 #ifndef OPENFLOW_VLAN_CONTROLLER_H
+#define OPENFLOW_VLAN_CONTROLLER_H
 
 #include "ns3/openflow-interface.h"
 
@@ -9,33 +9,33 @@
 #include <vector>
 #include <iostream>
 
-namespace ns3 {
+//namespace ns3 {
 
-namespace ofi {
+//namespace ofi {
 
-class VlanController : public LearningController
+class VlanController : public ns3::ofi::LearningController
 {
 public:
-	static TypeId GetTypeId (void);
-	TypeId GetInstanceTypeId () const;
+	static ns3::TypeId GetTypeId (void);
+	ns3::TypeId GetInstanceTypeId () const;
 
-	void SetVlanId (const Ptr<OpenFlowSwitchNetDevice> swtch, const int port, const uint16_t vid);
+	void SetVlanId (const ns3::Ptr<ns3::OpenFlowSwitchNetDevice> swtch, const int port, const uint16_t vid);
 
-	uint16_t GetVlanId (const Ptr<OpenFlowSwitchNetDevice> swtch, const int port);
+	uint16_t GetVlanId (const ns3::Ptr<ns3::OpenFlowSwitchNetDevice> swtch, const int port);
 
-	std::vector<int> EnumeratePorts (const Ptr<OpenFlowSwitchNetDevice> swtch, const uint16_t vid);
+	std::vector<int> EnumeratePorts (const ns3::Ptr<ns3::OpenFlowSwitchNetDevice> swtch, const uint16_t vid);
 
-	std::vector<int> EnumeratePortsWithoutInport (const Ptr<OpenFlowSwitchNetDevice> swtch, const int port, const uint16_t vid);
+	std::vector<int> EnumeratePortsWithoutInport (const ns3::Ptr<ns3::OpenFlowSwitchNetDevice> swtch, const int port, const uint16_t vid);
 
-	void ReceiveFromSwitch (Ptr<OpenFlowSwitchNetDevice> swtch, ofpbuf* buffer);
+	void ReceiveFromSwitch (ns3::Ptr<ns3::OpenFlowSwitchNetDevice> swtch, ofpbuf* buffer);
 
 
 private:
-	std::map<OpenFlowSwitchNetDevice*, std::map<int, uint16_t> > vid_map;
+	std::map<ns3::OpenFlowSwitchNetDevice*, std::map<int, uint16_t> > vid_map;
 };
 
-}
+//}
 
-}
+//}
 
 #endif /* OPENFLOW_VLAN_CONTROLLER_H */
--- vlan-controller-test-suite.cc
+++ vlan-controller-test-suite.cc
@@ -16,7 +16,7 @@
 
 #include "openflow-vlan-controller.h"
 
-using namespace ns3;
+//using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("VlanControllerTest");
 
@@ -52,52 +52,52 @@ SetTimeout (std::string value)
 int
 main (int argc, char *argv[])
 {	
-	CommandLine cmd;
-	cmd.AddValue ("verbose", "Verbose (turns on logging).", MakeCallback (&SetVerbose));
-	cmd.AddValue ("vlan", "Enable VLAN Mode", MakeCallback (&SetVlan));
-	cmd.AddValue ("timeout", "Learning Controller Timeout", MakeCallback (&SetTimeout));
+	ns3::CommandLine cmd;
+	cmd.AddValue ("verbose", "Verbose (turns on logging).", ns3::MakeCallback (&SetVerbose));
+	cmd.AddValue ("vlan", "Enable VLAN Mode", ns3::MakeCallback (&SetVlan));
+	cmd.AddValue ("timeout", "Learning Controller Timeout", ns3::MakeCallback (&SetTimeout));
 
 	cmd.Parse (argc, argv);
 
 	if (verbose)
 	{
-		LogComponentEnable ("VlanControllerTest", LOG_LEVEL_INFO);
-		LogComponentEnable ("OpenFlowInterface", LOG_LEVEL_INFO);
-		LogComponentEnable ("OpenFlowSwitchNetDevice", LOG_LEVEL_INFO);
+		ns3::LogComponentEnable ("VlanControllerTest", ns3::LOG_LEVEL_INFO);
+		ns3::LogComponentEnable ("OpenFlowInterface", ns3::LOG_LEVEL_INFO);
+		ns3::LogComponentEnable ("OpenFlowSwitchNetDevice", ns3::LOG_LEVEL_INFO);
 	}
 
 	NS_LOG_INFO ("Create nodes.");
-	NodeContainer terminals;
+	ns3::NodeContainer terminals;
 	terminals.Create (4);
 
-	NodeContainer csmaSwitch;
+	ns3::NodeContainer csmaSwitch;
 	csmaSwitch.Create (1);
 
 	NS_LOG_INFO ("Build Topology.");
-	CsmaHelper csma;
-	csma.SetChannelAttribute ("DataRate", DataRateValue (5000000));
-	csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
+	ns3::CsmaHelper csma;
+	csma.SetChannelAttribute ("DataRate", ns3::DataRateValue (5000000));
+	csma.SetChannelAttribute ("Delay", ns3::TimeValue (ns3::MilliSeconds (2)));
 
 	// Create the csma links, from each terminal to the switch.
-	NetDeviceContainer terminalDevices;
-	NetDeviceContainer switchDevices;
+	ns3::NetDeviceContainer terminalDevices;
+	ns3::NetDeviceContainer switchDevices;
 	for (int i = 0; i < 4; i++)
 	{
-		NetDeviceContainer link = csma.Install (NodeContainer (terminals.Get (i), csmaSwitch));
+		ns3::NetDeviceContainer link = csma.Install (ns3::NodeContainer (terminals.Get (i), csmaSwitch));
 		terminalDevices.Add (link.Get (0));
 		switchDevices.Add (link.Get (1));
 	}
 
 	// Create the switch netdevice, which will do the packet switching
-	Ptr<Node> switchNode = csmaSwitch.Get (0);
-	OpenFlowSwitchHelper swtch;
+	ns3::Ptr<ns3::Node> switchNode = csmaSwitch.Get (0);
+	ns3::OpenFlowSwitchHelper swtch;
 
 	if (vlan)
 	{
-		Ptr<VlanController> controller = CreateObject<VlanController> ();
+		ns3::Ptr<VlanController> controller = ns3::CreateObject<VlanController> ();
 		if (!timeout.IsZero ())
 		{
-			controller->SetAttribute ("ExpirationTime", TimeValue (timeout));
+			controller->SetAttribute ("ExpirationTime", ns3::TimeValue (timeout));
 		}
 		swtch.Install (switchNode, switchDevices, controller);
 	}
@@ -111,12 +111,12 @@ main (int argc, char *argv[])
 	#endif
 
 	// Add internet stack to the terminals
-	InternetStackHelper internet;
+	ns3::InternetStackHelper internet;
 	internet.Install (terminals);
 
 	// We have got the "hardware" in place. Now we need to add IP addresses.
 	NS_LOG_INFO ("Assign IP Addresses.");
-	Ipv4AddressHelper ipv4;
+	ns3::Ipv4AddressHelper ipv4;
 	ipv4.SetBase ("10.1.1.0", "255.255.255.0");
 	ipv4.Assign (terminalDevices);
 
@@ -124,30 +124,30 @@ main (int argc, char *argv[])
 	NS_LOG_INFO ("Create Applications.");
 	uint16_t port = 9; // Discard port
 
-	OnOffHelper onoff ("ns3::UdpSocketFactory", Address (InetSocketAddress (Ipv4Address ("10.1.1.2"), port)));
-	onoff.SetConstantRate (DataRate ("500kb/s"));
+	ns3::OnOffHelper onoff ("ns3::UdpSocketFactory", ns3::Address (ns3::InetSocketAddress (ns3::Ipv4Address ("10.1.1.2"), port)));
+	onoff.SetConstantRate (ns3::DataRate ("500kb/s"));
 
-	ApplicationContainer app = onoff.Install (terminals.Get (0));
+	ns3::ApplicationContainer app = onoff.Install (terminals.Get (0));
 
 	// Start the application
-	app.Start (Seconds (1.0));
-	app.Stop (Seconds (10.0));
+	app.Start (ns3::Seconds (1.0));
+	app.Stop (ns3::Seconds (10.0));
 
 	// Create ana optional packet sink to receive these packets
-	PacketSinkHelper  sink ("ns3::UdpSocketFactory", Address (InetSocketAddress (Ipv4Address::GetAny(), port)));
+	ns3::PacketSinkHelper  sink ("ns3::UdpSocketFactory", ns3::Address (ns3::InetSocketAddress (ns3::Ipv4Address::GetAny(), port)));
 	app = sink.Install (terminals.Get (1));
-	app.Start (Seconds (0.0));
+	app.Start (ns3::Seconds (0.0));
 
 	//
 	// Create a similar flow from n3 to n2, starting at time 1.1 seconds
 	//
-	onoff.SetAttribute ("Remote", AddressValue (InetSocketAddress (Ipv4Address  ("10.1.1.1"), port)));
+	onoff.SetAttribute ("Remote", ns3::AddressValue (ns3::InetSocketAddress (ns3::Ipv4Address  ("10.1.1.1"), port)));
 	app = onoff.Install (terminals.Get (3));
-	app.Start (Seconds (1.1));
-	app.Stop (Seconds (10.0));
+	app.Start (ns3::Seconds (1.1));
+	app.Stop (ns3::Seconds (10.0));
 
 	app = sink.Install (terminals.Get (2));
-	app.Start (Seconds (0.0));
+	app.Start (ns3::Seconds (0.0));
 
 	NS_LOG_INFO ("Configure Tracing.");
 
@@ -155,7 +155,7 @@ main (int argc, char *argv[])
 	// Configure tracing of all enqueue, dequeue, and NetDevice receive events.
 	// Trace output will be sent to the file "openflow-switch.tr"
 	//
-	AsciiTraceHelper ascii;
+	ns3::AsciiTraceHelper ascii;
 	csma.EnableAsciiAll (ascii.CreateFileStream ("openflow-vlan-switch.tr"));
 
 	//
@@ -169,8 +169,8 @@ main (int argc, char *argv[])
 	// Now, do the actual simulation.
 	//
 	NS_LOG_INFO ("Run Simulation.");
-	Simulator::Run ();
-	Simulator::Destroy ();
+	ns3::Simulator::Run ();
+	ns3::Simulator::Destroy ();
 	NS_LOG_INFO ("Done.");
 //	#else
 //	NS_LOG_INFO ("NS-3 OpenFlow is not enabled. Cannnot run simulation.");
